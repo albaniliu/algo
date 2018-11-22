210191dff189966845dbc2ab96ef84cf020517e2
liuzhao <liuzhao@mobike.com>
2018-11-22 22:31:41 +0800

    noip 2018 pj D

--- src/Main.java
+++ src/Main.java
@@ -1,89 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Main {
+    class Node {
+        Node left;
+        Node right;
+        int val;
+        public Node(int v) {
+            this.val = v;
+        }
+    }
 
+    int[] arrive;
+    Map<Integer, Long> dp = new HashMap<>();
+    int n, m;
     public void run(Scanner scanner) throws IOException {
 
-        int n = scanner.nextInt();
-        long d =  scanner.nextInt();
-        long k =  scanner.nextInt();
-        long[][] points = new long[n][2];
+        long ans = Long.MAX_VALUE;
+        n = scanner.nextInt();
+
+        Node[] nodes = new Node[n];
         for (int i = 0; i < n; i++) {
-            points[i][0] = scanner.nextInt();
-            points[i][1] = scanner.nextInt();
+            int v = scanner.nextInt();
+            nodes[i] = new Node(v);
         }
-        long low = -1;
-        long hi = 1000000000l;
-        while (low < hi) {
-            long mid = (hi + low + 1) / 2;
-            long min = Math.max(1, d - mid);
-            long max = d + mid;
-            Deque<Integer> queue = new LinkedList<>();
-            long[] scores = new long[n];
-            int l = n - 1;
-            int r = n - 1;
-            for (int i = n - 1; i >= 0; i--) {
-                long tmp = points[i][1];
-                while (l > i) {
-                    if (points[l][0] - points[i][0] >= min) {
-                        while (!queue.isEmpty() && scores[queue.getLast()] < scores[l]) queue.pollLast();
-                        queue.offerLast(l);
-                        l --;
-                    } else {
-                        break;
-                    }
-                }
-                while (r > l) {
-                    if (points[r][0] - points[i][0] > max) {
-                        while (!queue.isEmpty() && queue.getFirst() >= r) queue.pollFirst();
-                        r--;
-                    } else {
-                        break;
-                    }
-                }
-                if (queue.size() > 0 && scores[queue.getFirst()] > 0) {
-                    tmp += scores[queue.getFirst()];
-                }
-                scores[i] = tmp;
+        int[] parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            int l = scanner.nextInt() - 1;
+            int r = scanner.nextInt() - 1;
+            if (l != -1) {
+                nodes[i].left = nodes[l];
             }
-
-            while (l >= 0) {
-                if (points[l][0] >= min) {
-                    while (!queue.isEmpty() && scores[queue.getLast()] < scores[l]) queue.pollLast();
-                    queue.offerLast(l);
-                    l --;
-                } else {
-                    break;
-                }
-            }
-            while (r > l) {
-                if (points[r][0] > max) {
-                    while (!queue.isEmpty() && queue.getFirst() >= r) queue.pollFirst();
-                    r--;
-                } else {
-                    break;
-                }
-            }
-
-            if (queue.size() > 0 && scores[queue.getFirst()] >= k) {
-                hi = mid - 1;
-            } else {
-                low = mid;
-            }
+            if (r != -1) nodes[i].right = nodes[l];
         }
-        if (hi == 1000000000l) {
-            System.out.println(-1);
-        } else {
-            System.out.println(hi+1);
-        }
+        List<Integer> pre = new ArrayList<>();
 
     }
+
 
     public static void main(String[] args) throws NumberFormatException, IOException {
         Scanner scanner = new Scanner(System.in);
